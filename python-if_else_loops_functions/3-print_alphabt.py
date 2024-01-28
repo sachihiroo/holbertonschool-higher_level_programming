0c9ac12ba537ffa246edda11a98d5f8fa7e6794a
sachihiroo <saif3antar@gmail.com>
2024-01-28 18:37:13 +0100

    Print alpha except q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
-    if ch == "e" or ch == "q":
-        continue
-print("{}".format(chr(ch)), end="")
+    if ch not in [101,113]:
+        print("{}".format(chr(ch)), end="")
