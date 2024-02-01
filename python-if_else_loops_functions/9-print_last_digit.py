1ed980768e25d1a3fb77d359060871cf61f38d74
sachihiroo <saif3antar@gmail.com>
2024-02-01 17:26:40 +0100

    LAst digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    digit = abs(number) % 10
-    if digit > 9:
-        print(number)
-    elif 0 <= digit >= 9:
-        print(number)
-        return number
+    if number < 0:
+        number = abs(number) % 10
+    print("{}".format(number), end="")
+    return number
