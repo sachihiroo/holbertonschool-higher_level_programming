c06280f2800baebab9aad63f7d9154e05a402427
sachihiroo <saif3antar@gmail.com>
2024-02-02 15:00:08 +0100

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
         if num_args == 1:
             print("0 arguments.")
         elif num_args == 2:
-            print("{} argument:".format(num_args - 1))
-            print("argument 1: {}".format(args[1]))
+            print(f"{num_args - 1} argument:")
+            print(f"argument 1: {args[1]}")
         else:
-            print("{} arguments:".format(num_args - 1))
+            print(f"{num_args - 1} arguments:")
         for i in range(1, num_args):
-            print("{}: {}".format(i, args[i]))
+            print(f"{i}: {args[i]}")
 num_args()
