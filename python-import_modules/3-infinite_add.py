fdb06d4b2f9dc99115a95c6d431b308fad181b68
sachihiroo <saif3antar@gmail.com>
2024-02-04 20:49:35 +0100

    infinite_add

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,9 +2,7 @@
 if __name__ == "__main__":
     import sys
 
-    argv = sys.argv
-    num = len(argv)
     sum = 0
-    for idx in range(1, num):
-        sum += int(argv[idx])
+    for idx in range(1, len(sys.argv)):
+        sum += int(sys.argv[idx])
         print(sum)
