364ad850dad82f8a8830dd69a03fbb1873b54f06
sachihiroo <saif3antar@gmail.com>
2024-02-08 17:00:35 +0100

    etrieves an element

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for idx in range(len(my_list)):
-        if my_list[idx] < 0:
+        if idx < 0 or not my_list:
             return None
         if idx < (len(my_list)):
             return my_list[idx]
