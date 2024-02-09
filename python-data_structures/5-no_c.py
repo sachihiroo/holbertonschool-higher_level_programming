992eb921c9d44da63c54aca6103c84144724005d
sachihiroo <saif3antar@gmail.com>
2024-02-09 10:02:07 +0100

    Remove CHAR

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(my_string):
-        if my_string[i] != "C" or my_string[i] != "c":
-            print("{}".format(my_string[i]), end="")
+    text = ""
+    for i in my_string:
+        if i not in ["c", "C"]:
+            text = text + i
+    return text
