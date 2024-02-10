49b02833dade64c4d78c019bd9eeb7e512f18fbf
sachihiroo <saif3antar@gmail.com>
2024-02-10 16:16:57 +0100

    multiple_returns

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    frst_character = None
+    length = 0
+    if sentence:
+        frst_character = sentence[0]
+        length = len(sentence)
+    return (frst_character, length)
