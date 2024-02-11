6cf27a2877ca9b146d501961e359bd7fafedfb34
sachihiroo <saif3antar@gmail.com>
2024-02-11 16:24:33 +0100

    update_dict

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        return a_dictionary
-    else:
-        a_dictionary[key] = value
-        return a_dictionary
+    a_dictionary.update({str(key): value})
+    return a_dictionary
