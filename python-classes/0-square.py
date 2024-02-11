e22f933f69c0e7c15874cf74c26597fd34bc323a
sachihiroo <saif3antar@gmail.com>
2024-02-11 19:14:32 +0100

    Empty class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+"Empty Classes" are classes that don't have any attributes or methods,
+but they can still  be used as placeholders for future development.
+"""
+
+
 class Square:
+    """A class representing a square."""
+
     pass
