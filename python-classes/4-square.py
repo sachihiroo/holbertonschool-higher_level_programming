7ffa880b04f66051f4eceefb67e5256f3e3fc923
sachihiroo <saif3antar@gmail.com>
2024-02-12 18:22:21 +0100

    Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+"Empty Classes" are classes that don't have any attributes or methods,
+but they can still  be used as placeholders for future development.
+"""
+
+
+class Square:
+    """A class representing a square.
+    Attributes:
+    - size: int
+        The size of the square.
+
+    Methods:
+    - size: property
+        Getter method to retrieve the size of the square.
+    - size: setter
+        Setter method to set the size of the square.
+    - __init__(self, size=0):
+        Constructor to instantiate the Square class.
+    """
+
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        # Verifying if the size is greater than or equal to 0
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    # Public instance method
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size**2
+
+    # Property accessor methods (getter and setter)
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        self.__size = value
+
+    pass
