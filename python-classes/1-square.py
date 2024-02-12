d6612a998a02591bba1fdfb39354eabde1187cb9
sachihiroo <saif3antar@gmail.com>
2024-02-12 14:17:19 +0100

    square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+"Empty Classes" are classes that don't have any attributes or methods,
+but they can still  be used as placeholders for future development.
+"""
+
+
+class Square:
+    """A class representing a square."""
+    def __init__(self, size):
+        self.__size = size
+    pass
