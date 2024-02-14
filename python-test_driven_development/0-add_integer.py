19830c9d34ed141900f908eb5084e2354723204c
sachihiroo <saif3antar@gmail.com>
2024-02-14 18:01:08 +0100

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Add two integers. If only one argument is provided the other will be 98."""
-def add_integer(a, b=98):
 
+
+def add_integer(a, b=98):
     """
     arguments: a, b
     Return: int
     """
     if not (isinstance(a, int) or isinstance(a, float)):
-        raise TypeError("a must be an integer or float")
+        raise TypeError("a must be an integer")
     if not (isinstance(b, int) or isinstance(b, float)):
-        raise TypeError("b must be an integer or float")
+        raise TypeError("b must be an integer")
 
-    return (int(a)+int(b))
+    return int(a) + int(b)
