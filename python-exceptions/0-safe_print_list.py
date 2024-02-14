9bc5c819e3db229658f228df2ec2752594a0d3bd
sachihiroo <saif3antar@gmail.com>
2024-02-14 21:47:39 +0100

    Safe list printing

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    line = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            x = x + 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return line
