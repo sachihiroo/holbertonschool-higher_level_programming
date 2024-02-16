ef3353c89331819bb3f35132dc7db5b89fb1f6e1
sachihiroo <saif3antar@gmail.com>
2024-02-16 12:51:46 +0100

    Simple Rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+"Empty Classes" are classes that don't have any attributes or methods,
+but they can still  be used as placeholders for future development.
+"""
+
+
+class Rectangle:
+    """A class representing a Rectangle."""
+
+    pass
