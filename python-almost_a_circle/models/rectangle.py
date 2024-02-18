822f31acf002757721deb0589cd9fa965887a3ec
sachihiroo <saif3antar@gmail.com>
2024-02-19 00:11:36 +0100

    Rectangle upadated

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -36,7 +36,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if isinstance(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -49,7 +49,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if isinstance(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -62,7 +62,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if isinstance(value) != int:
+        if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -75,7 +75,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if isinstance(value) != int:
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
