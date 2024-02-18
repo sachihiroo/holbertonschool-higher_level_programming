d8c9b2c384dcff0795d5a702335b959e2bc1310b
sachihiroo <saif3antar@gmail.com>
2024-02-19 00:26:54 +0100

    Base updated

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -35,7 +35,6 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
-    
 
     @classmethod
     def save_to_file(cls, list_objs):
