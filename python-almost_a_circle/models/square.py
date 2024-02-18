46e3c8d58a657e8fc9df76a861eb20827d0fac18
sachihiroo <saif3antar@gmail.com>
2024-02-19 00:03:21 +0100

    square updated

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -48,13 +48,11 @@ class Square(Rectangle):
         if len(args) > 0:
             self.id = args[0]
         if len(args) > 1:
-            self.__width = args[1]
+            self.width = args[1]
         if len(args) > 2:
-            self.__height = args[2]
+            self.x = args[2]
         if len(args) > 3:
-            self.__x = args[3]
-        if len(args) > 4:
-            self.__y = args[4]
+            self.y = args[3]
         for key, value in kwargs.items():
             setattr(self, key, value)
 
