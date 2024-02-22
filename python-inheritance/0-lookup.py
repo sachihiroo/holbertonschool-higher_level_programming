26e037b6aee3ef440023ba31a45cdb0c197f9d2e
sachihiroo <saif3antar@gmail.com>
2024-02-22 22:04:47 +0100

    dir cmnd

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+return methods of object
+"""
+
+
 def lookup(obj):
-	"""Return a list of available attributes and methods of an object."""
-	return dir(obj)
+    """Return a list of available attributes and methods of an object."""
+    return dir(obj)
