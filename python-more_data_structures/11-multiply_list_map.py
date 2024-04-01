18f12e80745f1595835bb2dfebad2d82f861baf8
sachihiroo <saif3antar@gmail.com>
2024-04-01 01:02:00 +0100

    script

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-"""
-Write a function that returns a list with all
-values multiplied by a number
-"""
-
-
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda valx: valx*number, my_list))
